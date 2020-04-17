ffb8a38eddc69854fdf884c888d8c4930f83e0e4
Your Name <you@example.com>
2020-04-16 17:22:49 -0700

    added css to menu bar

--- comps/MenuBar/index.js
+++ comps/MenuBar/index.js
@@ -7,10 +7,10 @@ import {FaStickyNote} from 'react-icons/fa';
 
 const Menubar = () => <div>
 <div className="MenuContainer">
-    <div className="MenuDashboard"><FaTh /></div>
-    <div className="MenuImages"><FaRegImage /></div>
-    <div className="MenuLists"><FaList /></div>
-    <div className="MenuNotes"> <FaStickyNote /></div>
+    <div className="MenuItem"><div className="MenuDashboard"><FaTh size="28" color="#FFFFFF" /></div></div>
+    <div className="MenuItem"><div className="MenuImages"><FaRegImage size="28" color="#FFFFFF" /></div></div>
+    <div className="MenuItem"><div className="MenuLists"><FaList size="28" color="#FFFFFF" /></div></div>
+    <div className="MenuItem"><div className="MenuNotes"> <FaStickyNote size="28" color="#FFFFFF" /></div></div>
 </div>
 </div>
 
