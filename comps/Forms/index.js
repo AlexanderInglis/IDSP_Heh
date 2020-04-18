43a4cde694215d33aaa18f53c5a6291f3886a291
milanjot <milannagra@milans-MacBook-Pro.local>
2020-04-18 12:38:51 -0700

    progress commit

---
+++ comps/Forms/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './form.css';
+
+const SuForm = () => <div>
+    <div className="SuForm">
+        <input type="form" className="SuForm_input" />
+    </div>
+
+</div>
+
+export default SuForm;
---
+++ stories/SignUpForm.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SuForm from '../comps/Forms';
+
+export default {
+    title: "Sign Up Form",
+    component: SuForm,
+};
+
+export const HehSuForm = () => <SuForm />
