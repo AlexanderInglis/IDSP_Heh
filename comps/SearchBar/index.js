ddf630e16e1f68f136043a1b496decb1e953b134
milanjot <milannagra@milans-MacBook-Pro.local>
2020-04-19 03:24:23 -0700

    progress commit -- searchbar

---
+++ comps/SearchBar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './searchbar.css'
+
+const HehSearchBar = ({text, }) => <div>
+
+<div className="SB_container">
+
+</div>
+
+</div>
+
+HehSearchBar.defaultProps = {
+
+}
+
+export default HehSearchBar;
