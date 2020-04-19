76c9efcafca81b6684936eb0f89540764a48e518
milanjot <milannagra@milans-MacBook-Pro.local>
2020-04-19 04:47:24 -0700

    done searchbar- need to add function and avtr img

---
+++ stories/Searchbar.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HehSearchBar from '../comps/SearchBar'
+
+export default {
+  title: 'SearchBar',
+  component: HehSearchBar,
+};
+
+export const HehSBar = () => <HehSearchBar/>;
+
--- stories/searchbars.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HehSearchBar from '../comps/SearchBar'
-
-export default {
-    title: "Searchbar",
-    component: HehSearchBar,
-};
-
-export const HehSB = () => <HehSB />
