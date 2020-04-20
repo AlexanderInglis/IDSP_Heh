ff21e5cd631d1ddbfbdc99703745cb823eb23532
Your Name <you@example.com>
2020-04-19 21:08:23 -0700

    added images carousel

---
+++ comps/Carousel/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './carousel.css';
+import {FaArrowLeft} from 'react-icons/fa';
+import {FaArrowRight} from 'react-icons/fa';
+
+function MoveRight() {
+    //move images to the right
+
+}
+
+function MoveLeft() {
+    //move images to the right
+
+}
+
+const Carousel = () => <div>
+<div className="CaWrapper">
+    <ul>
+        <li><img src="https://via.placeholder.com/100x150"></img></li>
+        <li><img src="https://via.placeholder.com/100x150"></img></li>
+        <li><img src="https://via.placeholder.com/100x150"></img></li>
+        <li><img src="https://via.placeholder.com/100x150"></img></li>
+        <li><img src="https://via.placeholder.com/100x150"></img></li>
+    </ul>
+</div>
+</div>
+
+export default Carousel;
