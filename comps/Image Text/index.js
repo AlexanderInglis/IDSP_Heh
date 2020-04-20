24813405af8e92156e9250299af19ed935fce9aa
milanjot <milannagra@milans-MacBook-Pro.local>
2020-04-19 18:38:28 -0700

    image Text

--- comps/Image Text/index.js
+++ comps/Image Text/index.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import '../Image Text/imagetext.css'
 
+
+var StarIcon = require('../../comps/Image Text/plus.svg');
+
+const ImageText = ({ icon, backgroundColor, }) => <div>
+    <div class="iconbtn" style={{ backgroundColor: backgroundColor }}>
+        <img class="icon" src={icon} />
+    </div>
+</div>
+
+ImageText.defaultProps = {
+    icon: StarIcon,
+    backgroundColor: "#358E76",
+}
+
+export default ImageText;
---
+++ stories/imagetext.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImageText from '../comps/Image Text'
+
+export default {
+    title: "Image text header",
+    component: ImageText,
+};
+
+export const HehImageText = () => 
+<div>
+<HehImageText />
+</div>
