640e1a601b76239de29de1f3e5e8a499dd05f947
Your Name <you@example.com>
2020-04-19 23:04:34 -0700

    added textstyles

---
+++ stories/TextStyles.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Textstyle from '../comps/TextStyle'
+
+export default {
+    title: 'Text Styles',
+    component: Textstyle,
+  };
+
+  export const SampleText = () => <Textstyle />
