5cef4aeaab01ec034b86fa498e9b394c99a4d839
Your Name <you@example.com>
2020-04-19 19:58:29 -0700

    added logos as component

---
+++ comps/WithTextLogos/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WithText = require ('../../static/logos/logo-withtext.svg');
+
+const WithTextLogo = () => <div>
+    <img src={WithText}></img>
+</div>
+
+export default WithTextLogo;
---
+++ stories/logos.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NTLogo from '../comps/NoTextLogos';
+import WTLogo from '../comps/WithTextLogos';
+
+export default {
+    title: "Logos",
+    component: NTLogo,
+};
+
+export const HehLogo1 = () => <NTLogo />
+export const HehLogo2 = () => <WTLogo />
